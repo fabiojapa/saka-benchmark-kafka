ef0f6f9be8803eb4ffb1e8d98016aaa307d643ef
fabio saka <fabiojapa@gmail.com>
2020-05-16 17:11:07 -0300

    ok

---
+++ saka-springboot-kafka/src/test/java/com/saka/benchmark/kafka/sakaspringbootkafkaproducer/SakaSpringbootKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saka.benchmark.kafka.sakaspringbootkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakaSpringbootKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
